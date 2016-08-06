692d6f0b2e791702131cda7e2ac38b492ef4e17f
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-06 15:36:56 +0300

    Added a controllable spaceship sprite.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 10f;
+
+	void Update () {
+		MoveSpaceship();
+	}
+
+	void MoveSpaceship() {
+		if (Input.GetKey(KeyCode.LeftArrow)) {
+			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+		} else if (Input.GetKey(KeyCode.RightArrow)) {
+			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+		}
+	}
+}
