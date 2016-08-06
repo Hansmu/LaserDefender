967cc9e7751b032eb378b29985d3a80310ae4297
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-06 15:40:30 +0300

    Changed movement to use .left and .right of Vector3.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,9 +11,9 @@ public class PlayerController : MonoBehaviour {
 
 	void MoveSpaceship() {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.left * speed * Time.deltaTime;//Default function to move to the left
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
 	}
 }
