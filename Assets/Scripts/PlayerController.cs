755f4a103e99329293f12f514d924c1f5f536835
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-06 15:50:13 +0300

    Restricted player sprite to the playscreen.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,6 +4,20 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 10f;
+	public float padding = 1f;
+
+	private float xMin;
+	private float xMax;
+
+	void Start() {
+		float distance = transform.position.z - Camera.main.transform.position.z;
+
+		Vector3 leftMost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance)); //Relative coordinates here. 1,1 would mean at the top right. 0,0 would be at the bottom left.
+		Vector3 rightMost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+
+		xMin = leftMost.x + padding;
+		xMax = rightMost.x - padding;
+	}
 
 	void Update () {
 		MoveSpaceship();
@@ -15,5 +29,12 @@ public class PlayerController : MonoBehaviour {
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
 			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+
+		RestrictMovement();
+	}
+
+	void RestrictMovement() {
+		float newX = Mathf.Clamp(transform.position.x, xMin, xMax);
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 }
