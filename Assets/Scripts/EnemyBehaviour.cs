0bef2e6e1fcf479bff50745d84830d94454e82f9
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-12 15:10:51 +0300

    Enemy destruction on projectile collision.

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBehaviour : MonoBehaviour {
+
+	public float health = 150f;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		Projectile missile = collider.gameObject.GetComponent<Projectile>(); //GameObject we're colliding with.
+
+		if (missile) {
+			health -= missile.GetDamage();
+			missile.Hit();
+			if (health <= 0) {
+				Destroy(gameObject);
+			}
+		}
+	}
+}
