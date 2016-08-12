c34dedd8edc48d05a5f6a8b1631f471a946a378c
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-12 13:27:50 +0300

    Enemy spawning with gizmos.

---
+++ Assets/Scripts/Position.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Position : MonoBehaviour {
+
+	// Gizmos are things that aren't shown in the game, but shown to you as a developer.
+	void OnDrawGizmos() {
+		Gizmos.DrawWireSphere(transform.position, 1);
+	}
+}
