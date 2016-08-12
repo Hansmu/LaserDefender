86eabbf2d45a151370572a8ca4fbacafd9782eaf
Hans Miikael Uuspõld <hans.miikael@gmail.com>
2016-08-12 13:32:29 +0300

    Clarification on Gizmo sphere.

--- Assets/Scripts/Position.cs
+++ Assets/Scripts/Position.cs
@@ -5,6 +5,6 @@ public class Position : MonoBehaviour {
 
 	// Gizmos are things that aren't shown in the game, but shown to you as a developer.
 	void OnDrawGizmos() {
-		Gizmos.DrawWireSphere(transform.position, 1);
+		Gizmos.DrawWireSphere(transform.position, 1); //Draw a sphere around our GameObject with a radius of 1.
 	}
 }
